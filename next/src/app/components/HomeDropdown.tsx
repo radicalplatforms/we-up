d72d0aa2cc04c4d6bcf7b7dbd6c69c64ccf16762
Sengdao Inthavong <66700000+SengdowJones@users.noreply.github.com>
2024-04-06 17:55:23 -0500

    Add user circles and indicators as part of selected group

--- next/src/app/components/HomeDropdown.tsx
+++ next/src/app/components/HomeDropdown.tsx
@@ -13,7 +13,7 @@ const HomeDropdown: React.FC = () => {
         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         type="button"
       >
-    Dropdown button
+    Selected group
     <svg
       className="w-2.5 h-2.5 ms-3"
       aria-hidden="true"
@@ -43,22 +43,22 @@ const HomeDropdown: React.FC = () => {
           >
             <li>
               <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-                Dashboard
+                Asian Sensation
               </a>
             </li>
             <li>
               <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-                Settings
+                WildHacks
               </a>
             </li>
             <li>
               <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-                Earnings
+                Rockford
               </a>
             </li>
             <li>
               <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-                Sign out
+                Northwestern
               </a>
             </li>
           </ul>
