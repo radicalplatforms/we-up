471e8b0faf2b6bb4c8e3dfb31e6af0208835e431
Sengdao Inthavong <66700000+SengdowJones@users.noreply.github.com>
2024-04-06 19:27:13 -0500

    Edit chart for wakeup to show days of week

--- next/src/app/home/page.tsx
+++ next/src/app/home/page.tsx
@@ -1,8 +1,11 @@
+"use client";
 import HomeDropdown from '../components/HomeDropdown';
 import WeUpLogo from '../assets/weup.png';
 import Image from 'next/image';
+import WakeupChart from '../charts/WakeupChart';
 
 export default function Home() {
+      
     const MoonIndicator = () => (
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="top-0 start-7 absolute w-3.5 h-3.5 bg-white border-2 border-white dark:border-gray-800 rounded-full">
           <path fillRule="evenodd" d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z" clipRule="evenodd" />
@@ -58,6 +61,7 @@ export default function Home() {
                     <Image src={WeUpLogo} className="w-48 h-80" alt={''} />
                 </div>
             </div>
+            <WakeupChart />
         </main>
     );
 }
--- next/tailwind.config.ts
+++ next/tailwind.config.ts
@@ -19,7 +19,9 @@ const config: Config = {
     },
   },
   plugins: [
-    require("flowbite/plugin"),
+    require("flowbite/plugin")({
+      charts: true,
+    }),
   ],
 };
 export default config;
