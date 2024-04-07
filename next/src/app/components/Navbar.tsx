5a0b8dfa3aa4caee22542f8fc55a73dc4c626b7f
Radison Akerman <radison@me.com>
2024-04-07 04:08:22 -0500

    feat: navbar tweaks and api helpers

--- next/src/app/components/Navbar.tsx
+++ next/src/app/components/Navbar.tsx
@@ -4,7 +4,7 @@ import WeUpLogoBlack from '../assets/we-up-black.png';
 import Image from 'next/image';
 
 // @ts-ignore
-export default function Navbar({showBackHome}) {
+export default function Navbar({linkText, linkHref}) {
   return (
     <header className="bg-white">
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
@@ -13,15 +13,12 @@ export default function Navbar({showBackHome}) {
           <span className="sr-only">WeUp.</span>
           <Image src={WeUpLogoBlack} className="h-8 w-auto" alt=""/>
         </a>
-        <div className="flex flex-1 justify-end">{
-          showBackHome ?
-            <a href="/home" className="text-sm font-semibold leading-6 text-gray-900">
-              Back home <span aria-hidden="true">&rarr;</span>
-            </a> :
-            <a href="/profile" className="text-sm font-semibold leading-6 text-gray-900">
-              Profile <span aria-hidden="true">&rarr;</span>
+        <div className="flex flex-1 justify-end">
+          {linkHref && linkText && (
+            <a href={linkHref} className="text-sm font-semibold leading-6 text-gray-900">
+              {linkText} <span aria-hidden="true">&rarr;</span>
             </a>
-        }
+          )}
         </div>
       </nav>
     </header>
