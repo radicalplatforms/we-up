f2708601e0c624edb6c2dc5956db7243c4291a3a
Sengdao Inthavong <66700000+SengdowJones@users.noreply.github.com>
2024-04-07 02:34:20 -0500

    Mock image upload to up

--- next/src/app/home2/page.tsx
+++ next/src/app/home2/page.tsx
@@ -11,8 +11,14 @@ export default function Home2() {
     const [isUp, setIsUp] = useState(false);
     const handleClick = () => {
         setIsUp(true);
-      };
+    };
 
+    const handleImageUpload = (event: { target: { files: any[]; }; }) => {
+        const file = event.target.files[0];
+        if (file) {
+        setIsUp(true);
+        }
+    };
           
     const MoonIndicator = () => (
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="top-0 start-7 absolute w-3.5 h-3.5 bg-white border-2 border-white dark:border-gray-800 rounded-full">
@@ -32,7 +38,7 @@ export default function Home2() {
 
             {/* Fixed upload button */}
             <div className='fixed bottom-0 w-full flex justify-center items-center z-10'>
-                <button className='my-8 px-5 py-2 bg-blue-500 text-white text-sm font-bold tracking-wide rounded-full focus:outline-none shadow-lg hover:bg-blue-600 transition-colors duration-200'>
+                <button onClick={handleClick} className='my-8 px-5 py-2 bg-blue-500 text-white text-sm font-bold tracking-wide rounded-full focus:outline-none shadow-lg hover:bg-blue-600 transition-colors duration-200'>
                     I'm up!
                 </button>
             </div>
@@ -112,18 +118,23 @@ export default function Home2() {
                             </div>
 
                             {/* Upload function */}
+
                             <div className="my-2 flex items-center justify-center w-full">
                                 <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                                    {isUp ? (
+                                    <Image src={WeUpLogo} className="w-full h-64 object-cover rounded-lg" alt={''} />
+                                    ) : (
                                     <div className="flex flex-col items-center justify-center pt-5 pb-6">
                                         <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
                                         </svg>
                                         <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
                                         <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
                                     </div>
-                                    <input id="dropzone-file" type="file" className="hidden" />
+                                    )}
+                                    <input id="dropzone-file" type="file" className="hidden" onChange={handleImageUpload} />
                                 </label>
-                            </div> 
+                                </div>
 
                         </div>
                         <div id="dropdownDots" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-40 dark:bg-gray-700 dark:divide-gray-600">
