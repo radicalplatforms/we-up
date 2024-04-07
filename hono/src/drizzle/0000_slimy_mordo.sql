94d44fdf48785e6cea81547d3b9f2e9d4d0599bd
Vincent Do <85699604+vincentdo1@users.noreply.github.com>
2024-04-06 19:19:29 -0500

    feat: add user endpoints (#14)
    * create schema
    
    * format.
    
    * run migrations
    
    * format.
    
    * create user endpoints
    
    * nits
    
    * add relations
    
    * implement user endpoints
    
    * format
    
    * push test
    
    * push tests
    
    * decode
    
    * encode
    
    * test

---
+++ hono/src/drizzle/0000_slimy_mordo.sql
@@ -0,0 +1,45 @@
+CREATE TABLE IF NOT EXISTS "groups" (
+	"id" text PRIMARY KEY NOT NULL,
+	"rules" text
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "posts" (
+	"id" text PRIMARY KEY NOT NULL,
+	"user_id" text NOT NULL,
+	"photo_url" text,
+	"timestamp" timestamp with time zone DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "prompts" (
+	"id" text PRIMARY KEY NOT NULL,
+	"group_id" text NOT NULL,
+	"date" timestamp with time zone DEFAULT now() NOT NULL,
+	"text" text,
+	"user_id" text NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "users" (
+	"id" text PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"email" text NOT NULL,
+	"photo_url" text,
+	"wake_time" timestamp with time zone DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "users_to_groups" (
+	"user_id" text NOT NULL,
+	"group_id" text NOT NULL,
+	CONSTRAINT "users_to_groups_user_id_group_id_pk" PRIMARY KEY("user_id","group_id")
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "users_to_groups" ADD CONSTRAINT "users_to_groups_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "users_to_groups" ADD CONSTRAINT "users_to_groups_group_id_groups_id_fk" FOREIGN KEY ("group_id") REFERENCES "groups"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
