6399288c8dafa36ac394da551b6338a1b317045f
Sengdao Inthavong <66700000+SengdowJones@users.noreply.github.com>
2024-04-06 23:47:38 -0500

    Add rules

--- next/src/app/home2/page.tsx
+++ next/src/app/home2/page.tsx
@@ -49,28 +49,39 @@ export default function Home2() {
                 </div>
                 <div className="flex items-start gap-2.5">
                     {/* <Image src={WeUpLogo} className="w-8 h-8 rounded-full" alt={''} /> */}
-                    <div className="flex flex-col w-full max-w-[320px] leading-1.5 p-4 border-gray-200 bg-white rounded-e-xl rounded-es-xl dark:bg-gray-700">
+                    <div className="flex flex-col w-full max-w-[320px] leading-1.5 p-6 border-gray-200 bg-white rounded-xl dark:bg-gray-700">
                         <div className="flex items-center justify-center space-x-2 rtl:space-x-reverse">
                             {/* <span className="text-xl font-bold text-gray-900 dark:text-white">Asian Sensations</span> */}
                             <HomeDropdown />
                             {/* <span className="text-sm font-normal text-gray-500 dark:text-gray-400">11:46p</span> */}
                         </div>
-                        <p className="text-sm font-normal py-2.5 text-center text-gray-900 dark:text-white">Take a picture with your breakfast!</p>
-                        <div className="flex my-1 space-x-1 justify-center">
-                            <div className="relative">
-                                <Image src={WeUpLogo} className="w-8 h-8 rounded-full" alt={''} />
-                            </div>
-                            <div className="relative">
-                                <Image src={WeUpLogo} className="w-8 h-8 rounded-full" alt={''} />
-                            </div>
-                            <div className="relative">
-                                <Image src={WeUpLogo} className="w-8 h-8 rounded-full" alt={''} />
-                            </div>
-                            <div className="relative">
-                                <Image src={WeUpLogo} className="w-8 h-8 rounded-full" alt={''} />
-                            </div>
-                        </div>
-                        <span className="mt-1 text-center text-xs font-normal text-gray-500 dark:text-gray-400">caitlinclark, +5 have submitted.</span>
+
+                        {/* Upload function */}
+                        <div className="my-4 flex items-center justify-center w-full">
+                            <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                                <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                                    <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+                                    </svg>
+                                    <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                                    <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                                </div>
+                                <input id="dropzone-file" type="file" className="hidden" />
+                            </label>
+                        </div> 
+
+                <div>
+                    <h1 className="lowercase text-center mb-4 text-3xl font-bold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white"><mark className="px-2 text-white bg-blue-600 rounded dark:bg-blue-500">Rules</mark></h1>
+                </div>
+                <div className="items-center justify-between p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:flex dark:bg-gray-700 dark:border-gray-600">
+                    <time className="mb-1 text-xs font-normal text-gray-400 sm:order-last sm:mb-0">Rules</time>
+                    <div className="text-xs font-normal text-gray-500 dark:text-gray-300">                                1. All members must aim for at least 7-9 hours of sleep each night.<br/>
+                                2. No electronic devices in the bedroom after 9 PM.<br/>
+                                3. Daily sleep logs must be updated by 10 AM the following morning.<br/>
+                                4. Weekly group discussions on sleep quality and improvement strategies.<br/>
+                                5. Respect all member&apos; experiences and suggestions.<br/>
+                                6. Maintain a positive and supportive environment.</div>
+                </div>
                     </div>
                     <div id="dropdownDots" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-40 dark:bg-gray-700 dark:divide-gray-600">
                         <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownMenuIconButton">
@@ -96,7 +107,7 @@ export default function Home2() {
 
 
             {/* Prompt */}
-            <div className="pt-24">
+            <div className="pt-4">
                 <div>
                     <h1 className="lowercase text-center mb-8 text-3xl font-bold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Today&apos;s <mark className="px-2 text-white bg-blue-600 rounded dark:bg-blue-500">Prompt</mark></h1>
                 </div>
