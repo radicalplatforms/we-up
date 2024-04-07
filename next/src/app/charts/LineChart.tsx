51c448ad5884a3bcb176a065fa706b85c61112c0
Sengdao Inthavong <66700000+SengdowJones@users.noreply.github.com>
2024-04-06 23:47:38 -0500

    Add rules

--- next/src/app/charts/LineChart.tsx
+++ next/src/app/charts/LineChart.tsx
@@ -10,7 +10,7 @@ export default function LineChart() {
             {
                 label: "My First Dataset",
                 data: [65, 59, 80, 81, 56, 55, 40],
-                fill: false,
+                fill: true,
                 borderColor: "rgb(75, 192, 192)",
                 tension: 0.1
             }
