e99683552cdec3aaadd06d651b39489eb2018c96
Sengdao Inthavong <66700000+SengdowJones@users.noreply.github.com>
2024-04-06 23:16:46 -0500

    Add comment to show where upload is

--- next/src/app/upload/page.tsx
+++ next/src/app/upload/page.tsx
@@ -44,6 +44,7 @@ export default function Upload() {
                             {/* <span className="text-sm font-normal text-gray-500 dark:text-gray-400">11:46p</span> */}
                         </div>
 
+                        {/* Upload function */}
                         <div className="my-4 flex items-center justify-center w-full">
                             <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
                                 <div className="flex flex-col items-center justify-center pt-5 pb-6">
