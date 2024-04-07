907aa3691ae2814dc945ede62021bb1c721ef594
Radison Akerman <29184717+rak3rman@users.noreply.github.com>
2024-04-07 04:28:02 -0500

    feat: navbar tweaks and api helpers (#23)
    * feat: navbar tweaks and api helpers
    
    * fix: addressed nits

--- hono/src/services/users.ts
+++ hono/src/services/users.ts
@@ -8,17 +8,19 @@ const app = new Hono<{ Variables: Variables }>()
 
 app.get('/:email', injectDB, async (c) => {
   const param = c.req.param('email')
-  return c.json(
-    await c.get('db').query.users.findFirst({
-      where: eq(users.email, decodeURIComponent(param)),
-      with: {
-        usersToGroups: {
-          columns: { userId: false, groupId: false },
-          with: { group: true },
-        },
+  const res = await c.get('db').query.users.findFirst({
+    where: eq(users.email, decodeURIComponent(param)),
+    with: {
+      usersToGroups: {
+        columns: { userId: false, groupId: false },
+        with: { group: true },
       },
-    })
-  )
+    },
+  })
+  if (res) {
+    return c.json(res)
+  }
+  return c.notFound()
 })
 
 app.post('/', injectDB, async (c) => {
--- next/src/app/home2/page.tsx
+++ next/src/app/home2/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, Component } from 'react';
+import React, {useState, Component} from 'react';
 import WeUpLogo from '../assets/weup.png';
 import Image from 'next/image';
 import HomeDropdown from '../components/HomeDropdown';
@@ -10,24 +10,24 @@ import FeedInfo from '../components/FeedInfo';
 
 export default function Home2() {
 
-    const [isUp, setIsUp] = useState(false);
-    const handleClick = () => {
-        setIsUp(true);
-    };
-      
-    return (
-        <>
-            <Navbar showBackHome={false} />
-            {/* Container for full screen */}
-            <div className='flex min-h-screen flex-col items-center justify-between p-4 bg-gray-50 space-y-4'>
+  const [isUp, setIsUp] = useState(false);
+  const handleClick = () => {
+    setIsUp(true);
+  };
 
-                {/* Group Info + Prompt */}
-                <GroupInfo />
+  return (
+    <>
+      <Navbar linkText={"Profile"} linkHref={"/profile"}/>
+      {/* Container for full screen */}
+      <div className='flex min-h-screen flex-col items-center justify-between p-4 bg-gray-50 space-y-4'>
 
-                {/* Feed */}
-                <FeedInfo isUp={isUp} handleClick={handleClick}/>
-            </div>
-        </>
-    );
+        {/* Group Info + Prompt */}
+        <GroupInfo/>
+
+        {/* Feed */}
+        <FeedInfo isUp={isUp} handleClick={handleClick}/>
+      </div>
+    </>
+  );
 }
 
